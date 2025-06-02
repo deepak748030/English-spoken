52524097ddeb01844f1fdc93252759314be571bf
deepak748030 <deepakkushwah748930@gmail.com>
2025-06-02 15:24:00 +0530

    fix dumplicate gmail problem

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -3,10 +3,10 @@ import mongoose from 'mongoose';
 const userSchema = new mongoose.Schema({
     name: { type: String },
     mobileNo: { type: String, required: true, unique: true },
-    gmail: { type: String },
+    gmail: { type: String, unique: false, required: false },
     referCode: { type: String, unique: true, default: () => Math.random().toString(36).substr(2, 8).toUpperCase() },
     referredBy: { type: String, default: "" },
     isBlocked: { type: Boolean, default: false },
 }, { timestamps: true });
 
-export default mongoose.model('User', userSchema);
+export default mongoose.model('User', userSchema); 

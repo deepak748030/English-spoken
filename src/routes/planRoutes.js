b073b7a4d32c41ab7359952d14a99b0fd8ae4838
deepak748030 <deepakkushwah748930@gmail.com>
2025-06-09 15:45:20 +0530

    fix code

--- src/routes/planRoutes.js
+++ src/routes/planRoutes.js
@@ -9,7 +9,7 @@ import {
 
 const router = express.Router();
 
-router.post('/create', createPlan);           // POST /api/plans/create
+router.post('/', createPlan);           // POST /api/plans/create
 router.get('/', getAllPlans);                 // GET  /api/plans?type=one-to-one-class&planType=subscription
 router.get('/:id', getPlanById);              // GET  /api/plans/:id
 router.patch('/:id', updatePlan);             // PATCH /api/plans/:id

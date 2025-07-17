2bd50539a37c794fa8c7858bf0a88ccecbb62abe
Deepak kushwah <121612353+deepak748030@users.noreply.github.com>
2025-07-17 10:55:46 +0530

    Update userRoutes.js

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,11 +1,18 @@
-// src/routes/userRoutes.js
 import express from 'express';
-import { blockUser, getUsers, loginOrRegister, updateUser } from '../controllers/userController.js';
+import {
+  blockUser,
+  getUsers,
+  loginOrRegister,
+  updateUser,
+  updatePasswordIfAlreadySet
+} from '../controllers/userController.js';
 
 const router = express.Router();
 
 router.post('/loginOrRegister', loginOrRegister);
 router.patch('/:id', updateUser);
-router.get('/', getUsers)
+router.get('/', getUsers);
 router.patch('/:id/block', blockUser);
+router.post('/set-password', updatePasswordIfAlreadySet); // ✅ Route renamed to /set-password
+
 export default router;
